ec295c4eaced17f1eba2cc39d3438830ad026a1d
Jean Andreatta <60496134+jecsatta@users.noreply.github.com>
2024-01-26 11:11:52 -0300

    Create backspace.pas

---
+++ backspace.pas
@@ -0,0 +1,17 @@
+var
+   current, previous,next: TPoint;
+ begin
+   current := caller.LogicalCaretXY;
+
+   previous := current;
+   next:=current;
+
+   previous.x := previous.x -1;
+   next.x:=next.x+1;
+
+   if (caller.TextBetweenPoints[previous, current]='(') and (caller.TextBetweenPoints[current, next]=')') then ecDeleteChar;
+
+   if (caller.TextBetweenPoints[previous, current]='''') and (caller.TextBetweenPoints[current, next]='''') then ecDeleteChar;
+   ecDeleteLastChar;
+
+end.
